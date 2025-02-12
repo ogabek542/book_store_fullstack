f5b982694df83a9cb928103d6e4a3a6e49d6d8bf
Og'abek542 <123234816+ogabek542@users.noreply.github.com>
2025-02-12 11:02:07 +0500

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import {HiMiniBars3CenterLeft} from "react-icons/hi2"
+import {IoSearchOutline} from "react-icons/io5"
+
+
+
 
 const Navbar = () => {
   return (
     <header className='max-w-screen-2xl mx-auto px-4 py-6'>
         <nav className='flex justify-between items-center'>
           {/* left side */}
-          <div>Logo & Search bar</div>
-          {/* right side  */}
+          <div className='flex items-center'>
+            <Link to="/">
+              <HiMiniBars3CenterLeft className='size-6'/>
+            </Link>
+            {/* <==== search input ====> */}
+            <div>
+              <IoSearchOutline/>
+              <input type='text' placeholder='Search here' className="bg-[#eaeaea] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none "  />
+            </div>
+          </div>
+
+
+          {/* <==== right side ====> */}
           <div>
             nav items
           </div>
